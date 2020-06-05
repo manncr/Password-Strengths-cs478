13c1aa10c6993c8b8d751084ae3d39b00d4d5827
ryan cryar <ryan.cryar@global-formula-racing.com>
2020-06-04 17:47:04 -0700

    god why

--- src/checkers/complexity.js
+++ src/checkers/complexity.js
@@ -30,31 +30,80 @@ const determineStrength = password => {
 	// 			than 2 charsets. 0 otherwise
 	// d is the length of the substring that is a dictionary word
 	var special = "*|,\":<>[]{}`';()@&$#%";
+	var num = "1234567890";
+	var word = "abcdefghijklmnopqrstuvwzyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	var l = password.length;
 	if (l < 4) {
 		var feedback = "Password needs to be longer than 4 characters";
 		return {strength: 0, description: formatFeedback(feedback)}
 	}
-	var n = 1.0; // this is because we have the default charset = utf-8 we are gonna assume thats 1
-	var k = 1.0; // all of the substrings are in the same charset
+	var n = 0.0; // this is because we have the default charset = utf-8 we are gonna assume thats 1
+	var k = 0.0; // all of the substrings are in the same charset
+	var wordsub = [];
+	var numsub = [];
+	var specialsub = [];
+	var numstr = "";
+	var specialstr = "";
+	var wordstr = "";
 	var s = 0.0;
-	var p = 0.0
-	for (var i = 0; i < l; i++){
-		if (special.indexOf(password.charAt(i)) !== -1) {
-			s = 0.5
-			if (i === 0 || i === (l - 1)) {
-				p = 0.5; 
+	var p = 0.0;
+	var specialc,numc,wordc = 0;
+	for (var e = 0; e < l; e++){
+		if (num.indexOf(password.charAt(e)) !== -1){
+			numstr += password.charAt(e);
+			console.log(numstr);
+			if (num.indexOf(password.charAt(e + 1)) === -1){
+				numsub.push(numstr);
+				numstr = "";
+			}
+			if (numc === 0){
+				n+=1.0;
+				numc = 1;
+			}
+		}
+		if (word.indexOf(password.charAt(e)) !== -1){
+			wordstr += password.charAt(e)
+			if (word.indexOf(password.charAt(e + 1)) === -1){
+				wordsub.push(wordstr);
+				wordstr = "";
+			}
+			if (wordc === 0){
+				n+=1.0;
+				wordc = 1;
+			}
+		}
+		if (special.indexOf(password.charAt(e)) !== -1){
+			specialstr += password.charAt(e);
+			if (special.indexOf(password.charAt(e + 1)) === -1){
+				specialsub.push(specialstr);
+				specialstr = "";
+			}
+			if ((e === 0 || e === (l - 1)) && p === 0.0) {
+					p = 0.5; 
+			}
+			if (specialc === 0){
+				s = 0.5;
+				n+= 1.0;
 			}
 		}
 	}
 
+	k = numsub.length + wordsub.length + specialsub.length; 
+	console.log(numsub)
+	console.log(wordsub)
+	console.log(specialsub)
+	console.log(k);
+	console.log(n);
+
 	var textl = (text.length/30);
 	var d = 0.0;
-	for(var j = 0; j < textl; j++){
-		console.log('checking');
-		if (password === text[j]) {
-			d = text[j].length;
-			console.log("found");
+	for (var b = 0; b < wordsub.length; b++){
+		for(var j = 0; j < textl; j++){
+			console.log('checking');
+			if (wordsub[b] === text[j]) {
+				d = text[j].length;
+				console.log("found");
+			}
 		}
 	}
 
